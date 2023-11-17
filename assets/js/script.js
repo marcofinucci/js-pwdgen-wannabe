86e4dddbdf1a8a6d4a36b6ae486381ba3baece3d
marcofinucci <info@marcofinucci.com>
2023-11-17 09:56:31 +0100

    generate password in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+// Variables
+
+let yourName;
+let yourSurname;
+let yourColor;
+let yourPassword;
+const passwordEl = document.getElementById("password");
+
+// Ask name
+
+yourName = prompt("Quale è il tuo nome?");
+
+// Ask surname
+
+yourSurname = prompt("Quale è il tuo cognome?");
+
+// Ask color
+
+yourColor = prompt("Quale è il tuo colore preferito?");
+
+// Print password
+
+yourPassword = yourName + yourSurname + yourColor + "23";
+passwordEl.innerHTML = yourPassword;
